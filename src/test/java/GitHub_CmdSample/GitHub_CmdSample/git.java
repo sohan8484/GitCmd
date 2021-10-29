526893bd7325326e2bff4d1313db054192a9d506
sohan8484 <sohan84@outlook.com>
2021-10-29 13:08:43 +0530

    2 commit

--- src/test/java/GitHub_CmdSample/GitHub_CmdSample/git.java
+++ src/test/java/GitHub_CmdSample/GitHub_CmdSample/git.java
@@ -3,5 +3,7 @@ package GitHub_CmdSample.GitHub_CmdSample;
 public class git {
 public static void main(String[] args) {
 	System.out.println("git");
+	System.out.println("git2");
+	System.out.println("git3");
 }
 }
